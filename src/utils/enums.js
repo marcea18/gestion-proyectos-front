59894cd18442028a930424a8b3e542600ab16295
Marcela Aristizabal <marcea18@gmail.com>
2021-11-23 19:13:43 -0500

    iniciar autenticación

---
+++ src/utils/enums.js
@@ -0,0 +1,13 @@
+const Enum_Rol = {
+  ADMINISTRADOR: 'ADMINISTRADOR',
+  ESTUDIANTE: 'ESTUDIANTE',
+  LIDER: 'LIDER',
+};
+
+const Enum_EstadoUsuario = {
+  PENDIENTE: 'PENDIENTE',
+  AUTORIZADO: 'AUTORIZADO',
+  NO_AUTORIZADO: 'NO_AUTORIZADO',
+};
+
+export { Enum_Rol, Enum_EstadoUsuario };
